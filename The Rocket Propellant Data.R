c36f954bafe380f6b42edcddf9e966bd2ef7d1e6
Cherry <123944377+Cherry2k2@users.noreply.github.com>
2024-02-16 21:08:21 +0530

    The Rocket Propellant Data

---
+++ The Rocket Propellant Data.R
@@ -0,0 +1,71 @@
+# Performing Simple Linear Regression on the Rocket Propellant Dataset
+
+# Importing the Dataset
+dataset = read.csv("The Rocket propellant Data.csv")
+
+# Splitting the Dataset into Training and Testing set
+library(caTools) # loading caTools library
+library(ggplot2) # loading ggplot2
+set.seed(123) # setting a seed
+split = sample.split(dataset$Shear.strength, SplitRatio = 0.8)
+print(split) # printing the split 
+
+training_set = subset(dataset, split== TRUE) # creating the training set
+test_set = subset(dataset, split == FALSE) # creating the test set
+
+# Fitting the Simple Linear Regression Model using Training Set
+regressor = lm(formula = Shear.strength ~ Age.of.propellant, data = training_set) # fitting linear regression model using the training set
+print(regressor)
+
+# Predicting the Training Set Results
+y_train_pred = predict(regressor, training_set)
+print(y_train_pred)
+
+# Visualizing the Training Set Results
+ggplot() +
+  geom_point(aes(x = training_set$Age.of.propellant,
+                 y = training_set$Shear.strength),
+             colour = "red") +
+  geom_line(aes(x = training_set$Age.of.propellant,
+                y = y_train_pred),
+            colour = "blue") +
+  ggtitle("Regression Line Plot")+
+  xlab("Age of Propellant") + 
+  ylab("Shear Strength")
+
+# Predicting the Test Set Results
+y_test_pred = predict(regressor, test_set)
+print(y_test_pred)
+
+# Visualizing the Testing Set Results
+ggplot() +
+  geom_point(aes(x = test_set$Age.of.propellant,
+                 y = test_set$Shear.strength),
+             colour = "red") +
+  geom_line(aes(x = test_set$Age.of.propellant,
+                y = y_test_pred),
+            colour = "blue") +
+  ggtitle("Regression Line Plot")+
+  xlab("Age of Propellant") + 
+  ylab("Shear Strength")
+
+# calculating residuals and plotting them
+train_residual = y_train_pred - training_set$Shear.strength
+test_residual = y_test_pred - test_set$Shear.strength
+
+print(ggplot() +
+  geom_point(aes(x = 1:16,
+                 y = train_residual),
+             colour = "black") + 
+  geom_hline(yintercept = 0,
+             linetype = "solid",
+             color = "red"))
+
+ggplot() +
+  geom_point(aes(x = 1:4,
+                 y = test_residual),
+             colour = "black") + 
+  geom_hline(yintercept = 0,
+             linetype = "solid",
+             color = "red")
+
